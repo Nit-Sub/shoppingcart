effbe9b695c9b281d0470f2b85e1faceda2e1484
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-05-15 08:40:14 +0545

    cart

--- src/hook/AllRoutes.js
+++ src/hook/AllRoutes.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home"
+import Cart from "../components/Cart";
 
 const AllRoutes = () => {
   return (
     <>
     <Routes>
     <Route path="/" element={<Home/>}/>
+    <Route path="/cart" element={<Cart/>}/>
     </Routes>
     </>
   )
