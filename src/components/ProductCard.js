8cf284d5fa4b6b43ab3adc269877452f3c09dcba
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-05-18 22:24:08 +0545

    redux context

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,15 +1,26 @@
 import "./productcard.css";
-
-export const ProductCard = ({product}) => {
-  const {name, price, image} = product;
-
+import { useCart } from "../context/CartContext";
+import { useEffect, useState } from "react";
+export const ProductCard = ({ product }) => {
+  const { name, price, id, image } = product;
+  const [available, setAvailable] = useState(false)
+  const {  cartList,addToCart, removeFromCart} = useCart();
+  useEffect(() => {
+    const productIsInCart = cartList.find(cartItem=>cartItem.id === id);
+    if (productIsInCart) {
+      setAvailable(true);
+    }
+    else {
+      setAvailable(false);
+    }
+  },[cartList,id])
   return (
     <div className="productCard">
       <img src={image} alt={name} />
       <p className="name">{name}</p>
       <div className="action">
         <p>${price}</p>
-        <button>Add To Cart</button>
+        {available?(<button className="remove" onClick={()  => removeFromCart(product)}>Remove</button>):(<button onClick={() => addToCart(product)}>Add To Cart</button> )}
       </div>
     </div>
   )
