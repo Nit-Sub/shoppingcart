7ca5c8d02281b43ef37325af982439ab0d0445a0
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-05-20 15:22:09 +0545

    Update ProductCard.js

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,18 +1,32 @@
-import { useDispatch } from "react-redux";
-import { add } from "../store/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { add, remove } from "../store/cartSlice";
 import "./productcard.css";
+import { useEffect, useState } from "react";
 
-export const ProductCard = ({product}) => {
+export const ProductCard = ({ product }) => {
   const dispatch = useDispatch();
-  const {name, price, image} = product;
-
+  const { name, id, price, image } = product;
+  const [isInCart, setIsInCart] = useState(false);
+  const cartList = useSelector(state => state.cartState.cartList)
+  useEffect(() => {
+    const productInCart = cartList.find(item => item.id === id)
+    if (productInCart) {
+      setIsInCart(true)
+    }
+    else {
+      setIsInCart(false)
+    }
+  }, [id, cartList])
   return (
     <div className="productCard">
       <img src={image} alt={name} />
       <p className="name">{name}</p>
       <div className="action">
         <p>${price}</p>
-        <button onClick={() => dispatch(add(product))}>Add To Cart</button>
+        {
+          isInCart ? (<button className="remove" onClick={() => dispatch(remove(product))}>Remove</button>)
+            : (<button onClick={() => dispatch(add(product))}>Add To Cart</button>)
+        }
       </div>
     </div>
   )
